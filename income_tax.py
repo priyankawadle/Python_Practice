8c80db9b730acbfe298d182b2bd08004a4007ff4
priyankawadle <wadle98priyanka@gmail.com>
2025-05-15 21:20:33 +0530

    doubt_session_4

--- income_tax.py
+++ income_tax.py
@@ -1,18 +1,40 @@
+import datetime as dt
 class TaxPayer:
-    def __init__(self,name,PAN,income):
+    def __init__(self,name,PAN,income,dob):
         self.__name = name
         self.__PAN = PAN
         self._income = income
+        self.__dob = dob
+        self.__age = self.calculate_age()
     
     def print_profile(self):
         print("Name is: ",self.__name)
         print("PAN is: ",self.__PAN)
         print("Income is: ",self._income)
+        print("dob of person is: ",self.__dob)
+        print("age of person is: ",self.__age)
+
+    def calculate_age(self):
+        dobarr = self.__dob.split("/")
+        dob_date = dobarr[0]
+        dob_month = dobarr[1]
+        dob_year = dobarr[2]
+        today = dt.datetime.today()
+        today_date,today_month,today_year = today.day,today.month,today.year
+        if today_month == int(dob_month) and today_date == int(dob_date):
+           age = today_year- int(dob_year) 
+        else :
+            age = today_year- int(dob_year) 
+            age -= 1
+           
+        return age
+
+
 
 
 class TaxCalculator(TaxPayer):
-    def __init__(self,name,PAN,income):
-        super().__init__(name,PAN,income)
+    def __init__(self,name,PAN,income,dob):
+        super().__init__(name,PAN,income,dob)
         self.taxable_income = self._income
 
     @property
@@ -34,6 +56,12 @@ class TaxCalculator(TaxPayer):
         print("Taxable income: ",self.taxable_income)
         print("Total tax: ",self.tax)
 
+    def __sub__(self,amt):
+        if self.taxable_income < amt:
+            print( "Enter amount less than taxable amount")
+        else:
+            self.taxable_income -= amt
+
 class SeniorCitizenTaxCalculator(TaxCalculator):
     
     @property
@@ -47,5 +75,7 @@ class SeniorCitizenTaxCalculator(TaxCalculator):
         else:
             tax = (200000 * 0.05) + (500000 * 0.10) + (self.taxable_income - 1000000) * 0.20
         return tax
+
+
 
 
